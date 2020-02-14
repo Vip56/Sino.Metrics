e18b537ea8024e666a17c01155525c7f96dee49c
姚正发 <976691141@qq.com>
2020-02-14 14:38:18 +0800

    增加测试

---
+++ test/TestWebApp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using App.Metrics;
+using App.Metrics.AspNetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Sino.Metrics.Formatters.Prometheus;
+
+namespace TestWebApp
+{
+    public class Program
+    {
+        public static IMetricsRoot Metrics { get; set; }
+
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            Metrics = AppMetrics.CreateDefaultBuilder()
+                .OutputMetrics.AsPrometheusPlainText()
+                .Build();
+
+            return WebHost.CreateDefaultBuilder(args)
+                .ConfigureMetrics(Metrics)
+                .UseMetrics(options =>
+                {
+                    options.EndpointOptions = endpointsOptions =>
+                    {
+                        endpointsOptions.MetricsTextEndpointOutputFormatter = Metrics.OutputMetricsFormatters.OfType<MetricsPrometheusTextOutputFormatter>().First();
+                    };
+                })
+                .UseStartup<Startup>()
+                .Build();
+        }
+    }
+}
